d09ee4fb84b7e7d02d84bb93e26daae88da11966
singrongchiu <singrongchiu@utexas.edu>
2023-09-19 14:44:05 -0500

    changed formatting, paging, and subprocess no concurrent

--- find_bash.sh
+++ find_bash.sh
@@ -1,16 +1,30 @@
 #! /bin/bash
-# ${1} is ssh clone link, ${2} is repo name, ${3} is output txt file name
+# ${1} is ssh link to repo, ${2} is repo name, 
+# ${3} is http link to repo, ${4} is star count, ${5} is output text file
 
-echo ${1}
+# output: http_link,SHA,stars,permanent github URL
 
 git clone ${1}
 
+# x=`find ./${2} -name "*.sh"`
 x=`find ./${2} -name "*.sh"`
 echo "find .sh files:" 
 echo $x
 
-echo "${1} ${2}" >> ${3}
-echo $x >> ${3}
+cd ${2}
+sha=`git log --pretty=format:'%h' -n 1`
+sha_long=`git log --pretty=format:'%H' -n 1`
+cd ..
+
+shfiles=$(echo $x | tr " " "\n")
+
+full_title_length=${#2}+2
+
+for addr in $shfiles
+do
+    filepath="${addr:${full_title_length}:}"
+    echo "${3},${sha},${4},`${3}+${sha_long}+${filepath}`" >> ${5}
+done
 
 rm -rf ${2}
 
--- get_repos.py
+++ get_repos.py
@@ -2,7 +2,7 @@ import requests
 import json
 import subprocess
 
-q = "q=language:java&sort=stars&order=desc&per_page=100"
+q = "q=language:java&sort=stars&order=desc&per_page=100&page=1"
 url = "https://api.github.com/search/repositories?" + q
 
 print(url)
@@ -10,7 +10,11 @@ print(url)
 r = requests.get(url)
 response = json.loads(r.text) 
 urlname_lst = []
-urlname_lst = [(e['ssh_url'], e['name']) for e in response['items']]
+urlname_lst = [(e['ssh_url'], e['name'], e['html_url'], e['stargazers_count']) for e in response['items']]
+
+# subprocess.run(["bash", "./find_bash.sh", "%s" % (urlname_lst[0][0]), "%s" % (urlname_lst[0][1]),
+#                 "%s" % (urlname_lst[0][2]), "%s" % (urlname_lst[0][3]), "output.txt"])
 
 for tupleUrl in urlname_lst:
-    subprocess.Popen(["bash", "./find_bash.sh", "%s" % (tupleUrl[0]), "%s" % (tupleUrl[1]), "output.txt"])
+    subprocess.run(["bash", "./find_bash.sh", "%s" % (tupleUrl[0]), "%s" % (tupleUrl[1]),
+                "%s" % (tupleUrl[2]), "%s" % (tupleUrl[3]), "output.txt"])
